a1502ee93488a3be25a36411b883570652d3990a
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-12-04 12:58:10 -0300

    #Commit - eureka server e Accounts

--- src/main/java/com/tuyo/accounts/model/Accounts.java
+++ src/main/java/com/tuyo/accounts/model/Accounts.java
@@ -30,18 +30,6 @@ public class Accounts {
 	private long accountNumber;
 	@Column(name="account_type")
 	private String accountType;
-
-
-
-
-
-
-
-
-
-
-
-	
 	@Column(name = "branch_address")
 	private String branchAddress;
 	@Column(name = "create_dt")
